f2e3b1dc40512db055e9c74119eb7fb83c2cc98a
sohailmomin712 <sohailmomin053@gmail.com>
2024-06-25 16:59:19 +0530

    started fetching api

---
+++ script.js
@@ -0,0 +1,34 @@
+function getWeather() {
+  const curreWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+  const apiKey = "ebb64b2add17e1faba6af2d56e05037e";
+  const city = document.getElementById("city").value;
+
+  if (!city) {
+    alert("please enter a city");
+    return;
+  }
+
+  fetch(curreWeatherUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      displayWeather(data);
+    })
+    .catch((error) => {
+      console.error("error fetching current weather data:", error);
+      alert("error fetching current weather data. Please try again.");
+    });
+
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      displayHourlyForecast(data.list);
+    })
+    .catch((error) => {
+      console.error("error fetching hourly forecast data:", error);
+      alert("error fetching hourly forecast data. Please try again.");
+    });
+}
+
+
