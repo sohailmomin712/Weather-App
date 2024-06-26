a1a5094698027200bc4f79e3f57c3bf65f94a640
sohailmomin712 <sohailmomin053@gmail.com>
2024-06-26 12:20:41 +0530

    #weather app using then and catch methode complete

--- script.js
+++ script.js
@@ -36,4 +36,30 @@ function displayWeather(data) {
   const weatherInfoDiv = document.getElementById("weather-info");
   const weatherIcon = document.getElementById("weather-icon");
   const hourlyForecastDiv = document.getElementById("hourly-forecast");
+
+  //clear previous content//
+  weatherInfoDiv.innerHTML = "";
+  hourlyForecastDiv.innerHTML = "";
+  tempDivInfo.innerHTML = "";
+
+  if (data.cod === "404") {
+    weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
+  } else {
+    const cityName = data.name;
+    const temperature = Math.round(data.main.temp - 273.15); //converted into celsisus//
+    const description = data.weather[0].description;
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+    const temperatureHTML = `<p>${temperature}C</p>`;
+    const weatherHTML = `<p>${cityName}</p>
+                        <p>${description}</p>`;
+
+    tempDivInfo.innerHTML = temperatureHTML;
+    weatherInfoDiv.innerHTML = weatherHTML;
+    weatherIcon.src = iconUrl;
+    weatherIcon.alt = description;
+
+    showImage();
+  }
 }
